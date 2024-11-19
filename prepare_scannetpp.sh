448ca3a5a361790de9364e5b12109969d2d0372e
RyanPham19092002 <phamthanhphat711@gmail.com>
2024-11-19 03:17:30 +0000

    first commit

---
+++ prepare_scannetpp.sh
@@ -0,0 +1,4 @@
+python prepare_scannetpp.py \
+    --path /home/vinai/Workspace/phat-intern-dev/VinAI/Fisheye-GS/scannetpp/c9bf4c8b62/dslr \
+    --src images \
+    --dst image_undistorted_fisheye 
